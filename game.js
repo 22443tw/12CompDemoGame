d4b429de60f0e76ae85e4a918cc7ab9163fb7b5d
22443tw <124744326+22443tw@users.noreply.github.com>
2024-02-29 15:13:04 +1300

    Update game.js

--- game.js
+++ game.js
@@ -1,32 +1,36 @@
 /*******************************************************/
 // Written by Tristan
 /*******************************************************/
+
+
 function setup() {
-  console.log("setup: ");
   cnv = new Canvas(1000,800)
+  
   world.gravity.y = 10;
+  
   rectangle = new Sprite(100, 500, 80, 80,'d');
   rectangle.colour = 'white';
-  rectangle.rotationSpeed = 0; 
-  rectangle.friction = 0;
-  rectangle.drag = 0;
+  
   platform_1 = new Sprite(500, 750, 1000, 300, 'k');
   platform_1.colour = 'black';
+  
+  obstacle = new Sprite(900, 500 , 80, 80, 'd');
+  obstacle.color = color("yellow");
+  obstacle.vel.x = -10;
 }
 
 document.addEventListener("keydown", 
-  function(event) {
-  if (event.code === 'ArrowUp') {
-      rectangle.vel.y = 33;
-  }
-  else{ 
-       rectangle.vel.y = 0;
-}});
+    function(event) {
+        console.log("Key pressed!"+rectangle.y);
+            if(rectangle.y > 558 ){
+            console.log("Key pressed!");
+            rectangle.vel.y = -9;
+    }
+});
 
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-  clear();
   background('pink');
 }
