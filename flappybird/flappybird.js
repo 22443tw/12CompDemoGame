7ed200ab5ffe04a294674adf82222a8d71370e33
22443tw <124744326+22443tw@users.noreply.github.com>
2024-03-11 09:13:27 +1300

    Update flappybird.js

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -1,1 +1,10 @@
+console.log("%c t01_create_sprite", "color: blue;");
 
+const SCREEN_WIDTH = 400;
+const SCREEN_HEIGHT = 800;
+const PLAYER_RADIUS = 25;
+var score = 0;
+
+var player;
+var screenSelector = "start";  
+var obstacles;
